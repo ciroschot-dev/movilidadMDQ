35b6f5715fdf53cf679a0216e44d05bc02284e81
ciroschot-dev <cirschot@gmail.com>
2026-04-17 11:26:13 -0300

    primer commit

---
+++ src/main/java/com/example/movilidadmdq/MovilidadMdqApplication.java
@@ -0,0 +1,15 @@
+package com.example.movilidadmdq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovilidadMdqApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(MovilidadMdqApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/movilidadmdq/MovilidadMdqApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.movilidadmdq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovilidadMdqApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
