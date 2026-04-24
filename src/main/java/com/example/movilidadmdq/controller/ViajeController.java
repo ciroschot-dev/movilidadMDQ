004f7faa8bbe6c8d142572cb9e11a4d0bba61bc7
ciroschot-dev <cirschot@gmail.com>
2026-04-24 13:32:40 -0300

    Agregue soporte para CORS en ViajeController

--- src/main/java/com/example/movilidadmdq/controller/ViajeController.java
+++ src/main/java/com/example/movilidadmdq/controller/ViajeController.java
@@ -5,20 +5,14 @@ import com.example.movilidadmdq.dto.OpcionTransporteResponse;
 import com.example.movilidadmdq.service.ViajeService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
-import static java.util.Arrays.stream;
-
 
 @RequiredArgsConstructor
 @RequestMapping("/viajes")
 @RestController
+@CrossOrigin(origins = "http://localhost:5173")
 
 public class ViajeController
 {
