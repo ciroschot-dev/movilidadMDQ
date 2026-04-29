f0b8a0abd77dde6fdbec4515c7bb5d3fdf73e8e1
anibaldb <128552893+anibaldb@users.noreply.github.com>
2026-04-28 23:33:19 -0300

    cambios a1.1

--- src/main/java/com/example/movilidadmdq/MovilidadMdqApplication.java
+++ src/main/java/com/example/movilidadmdq/MovilidadMdqApplication.java
@@ -4,6 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class })
 public class MovilidadMdqApplication
 {
