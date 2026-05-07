a408d6c7fdff0782a3f62251b23fc8a7c1a5568e
tiagofueyovuillermoz-beep <tiagofueyovuillermoz@gmail.com>
2026-05-07 19:34:13 -0300

    cambio de clave google maps

--- frontend-demo/env.js
+++ frontend-demo/env.js
@@ -1,5 +1,5 @@
 // Configuración de la Demo
 const ENV = {
-    GOOGLE_MAPS_KEY: "AIzaSyAEE2PByHc_jvuxcnD4RG05qY4By0v9_tg",
+    GOOGLE_MAPS_KEY: "AIzaSyAowUupDwyKoXK7fyU4hGT3R6xKQ-pe70Q",
     API_URL: "http://localhost:8080/viajes/calcular"
 };
